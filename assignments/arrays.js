2992cf44dfb3fed2fa6060b25217307d1f5e96d2
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-13 16:14:26 -0400

    arrays3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -79,6 +79,14 @@ console.log(inventory[inventory.length - 1].car_make, inventory[inventory.length
 let carModels = [];
 console.log();
 
+for (let i = 0; i < inventory.length; i++) {
+    carModels.push(inventory[i].car_model);
+}
+
+carModels.sort();
+
+console.log(carModels);
+
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
