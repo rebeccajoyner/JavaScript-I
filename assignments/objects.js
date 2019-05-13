f1fea0cd95f64505a863ad177c7e07f70b1ce877
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-13 18:52:28 -0400

    .

--- assignments/objects.js
+++ assignments/objects.js
@@ -83,7 +83,7 @@ console.log(int5.name);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-speak = function () {
+int2.speak = function () {
   return ('Hello, my name is ' + this.name);
 }
 console.log(int2.speak());
