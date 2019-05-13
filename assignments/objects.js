55f26dc02b0737774c87db0e35d2867b8037f198
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-13 17:43:52 -0400

    objects 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -84,6 +84,11 @@ console.log(int5.name);
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
+int5.multiplyNum = function (num1, num2) {
+  return num1 * num2;
+}
+console.log(int5.multiplyNum(3, 4));
+
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
